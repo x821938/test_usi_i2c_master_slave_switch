1174f1c531baf8f719a03f846f1c4069a15014d4
Alex Jensen <alex@offline.dk>
2017-10-21 09:08:49 +0200

    Working version

--- attiny/attiny.ino
+++ attiny/attiny.ino
@@ -1,5 +1,14 @@
-#include <USIWire.h>
+// Bitbanging with softwire
+#define SDA_PORT PORTB
+#define SDA_PIN 0
+#define SCL_PORT PORTB
+#define SCL_PIN 2
 
+#include <SoftWire.h>
+SoftWire bbWire = SoftWire();
+
+// USIwire for slave
+#include <USIWire.h>
 #define MY_SLAVE_ADDR			0x10
 #define VEML6070_ADDR_H			0x39
 #define VEML6070_ADDR_L			0x38
@@ -10,8 +19,8 @@ volatile uint8_t lightValue = 0;
 volatile byte commandReceived = 0;
 
 
-void setup()
-{
+void setup() {
+	// start USI slave
 	Wire.begin( MY_SLAVE_ADDR );
 	Wire.onRequest( requestEvent );
 	Wire.onReceive( receiveEvent );
@@ -20,13 +29,16 @@ void setup()
 }
 
 
-void loop()
-{
+void loop() {
 	// This is my ultimate goal to switch between master and slave.
 	if ( commandReceived == 'L' ) {
-		Wire.end();						// Stop being a slave
+		Wire.end();						// Stop being an USI slave
 		readFromLightSensor();			// Get data
-		Wire.begin( MY_SLAVE_ADDR );	// And become slave again
+
+		// Start USI slave again
+		Wire.begin( MY_SLAVE_ADDR );
+		Wire.onRequest( requestEvent );
+		Wire.onReceive( receiveEvent );
 		commandReceived = 0;			// Reset the command - we will be waiting for the next.
 	}
 }
@@ -44,17 +56,17 @@ void receiveEvent( int howMany ) {
 }
 
 
-/* In master mode: get data from a very simple VEML6070 UV sensor */
+/* In bitbang master mode: get data from a very simple VEML6070 UV sensor */
 void readFromLightSensor() {
-	Wire.begin();			// Become master
+	bbWire.begin();			// Become master
 
-	Wire.beginTransmission( VEML6070_ADDR_L );
-	Wire.write( 0x04 );		// Sensor setup
-	Wire.endTransmission();
+	bbWire.beginTransmission( VEML6070_ADDR_L );
+	bbWire.write( 0x04 );		// Sensor setup
+	bbWire.endTransmission();
 
-	Wire.requestFrom( VEML6070_ADDR_L, 1 );
-	lightValue = Wire.read(); // Get LSB of light value - enough for test
+	bbWire.requestFrom( VEML6070_ADDR_L, 1 );
+	lightValue = bbWire.read(); // Get LSB of light value - enough for test
 
-	// Visual feedback. By using a flashlight on the VEML sensor, I can turn off the TEST LED.
-	digitalWrite( TEST_LED, lightValue > LIGHT_TRIGGER_VALUE ? LOW : HIGH ); 
+							  // Visual feedback. By using a flashlight on the VEML sensor, I can turn off the TEST LED.
+	digitalWrite( TEST_LED, lightValue > LIGHT_TRIGGER_VALUE ? LOW : HIGH );
 }
